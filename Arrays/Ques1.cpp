fbdca174e3bd3e76d865a13063eea388ad1c3a08
Christo Jeeson <68178794+Christo-CJ@users.noreply.github.com>
2025-01-30 23:57:11 +0530

    Update Ques1.cpp

--- Arrays/Ques1.cpp
+++ Arrays/Ques1.cpp
@@ -9,7 +9,7 @@ Output: 34
 Explanation: The largest element of the array is 35 and the second largest element is 34. 
 Input: arr[] = [10, 5, 10] 
 Output: 5 
-Explanation: The largest element of the array is 10 and the second largest element is 5.
+Explanation: The largest element of the array is 10 and the second largest element is 5. 
 Input: arr[] = [10, 10, 10]
 Output: -1
 Explanation: The largest element of the array is 10 and the second largest element does not exist.
