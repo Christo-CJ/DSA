305a8ce9649cc79a165e62a12db4f9ff823d17d8
Christo Jeeson <68178794+Christo-CJ@users.noreply.github.com>
2025-02-09 23:56:16 +0530

    Update Ques4.cpp

--- Arrays/Ques4.cpp
+++ Arrays/Ques4.cpp
@@ -4,7 +4,7 @@ Given an array arr[]. Rotate the array to the left (counter-clockwise direction)
 Note: Consider the array as circular. 
 
 Examples :
-
+  
 Input: arr[] = [1, 2, 3, 4, 5], d = 2
 Output: [3, 4, 5, 1, 2]
 Explanation: when rotated by 2 elements, it becomes 3 4 5 1 2.
