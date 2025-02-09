c980ad75cad7b658bdcaa022f6f9eb1cdf2804aa
Christo Jeeson <68178794+Christo-CJ@users.noreply.github.com>
2025-02-09 23:55:16 +0530

    Create Ques4.cpp

---
+++ Arrays/Ques4.cpp
@@ -0,0 +1,89 @@
+/*
+Given an array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned change in the array in place.
+
+Note: Consider the array as circular.
+
+Examples :
+
+Input: arr[] = [1, 2, 3, 4, 5], d = 2
+Output: [3, 4, 5, 1, 2]
+Explanation: when rotated by 2 elements, it becomes 3 4 5 1 2.
+Input: arr[] = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20], d = 3
+Output: [8, 10, 12, 14, 16, 18, 20, 2, 4, 6]
+Explanation: when rotated by 3 elements, it becomes 8 10 12 14 16 18 20 2 4 6.
+Input: arr[] = [7, 3, 9, 1], d = 9
+Output: [3, 9, 1, 7]
+Explanation: when we rotate 9 times, we'll get 3 9 1 7 as resultant array.
+Constraints:
+1 <= arr.size(), d <= 105
+0 <= arr[i] <= 105
+*/
+
+
+class Solution {
+  public:
+  void reverse(vector<int>&arr,int i,int j)
+  {
+      while(i<j)
+      {
+      swap(arr[i],arr[j]);
+      i++;
+      j--;
+  }
+  }
+
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    void rotateArr(vector<int>& arr, int d) {
+        // code here
+       
+        
+       int size=arr.size();
+       d=d%size;
+       if(d==0)
+       return;
+       reverse(arr,0,d-1);
+       reverse(arr,d,size-1);
+       reverse(arr,0,size-1);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        // calling rotateArr() function
+        ob.rotateArr(arr, d);
+
+        // printing the elements of the array
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
